056f34f4f03d513f7bf81b70f2b2dd72447b2e69
Vanessa <vanessa.co403@gmail.com>
2023-03-06 18:02:08 -0500

    review components

---
+++ src/components/ReviewCard.js
@@ -0,0 +1,14 @@
+function ReviewCard({name, comment, rating }){
+
+ 
+    return(
+        <div>
+            <h1>{name}</h1>
+            <h2>{comment}</h2>
+            <h2>{rating}</h2>
+         
+        </div>
+    )
+}
+
+export default ReviewCard;
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,10 +1,63 @@
-
+import React, {useState, useEffect} from "react";
+import ReviewCard from "./ReviewCard";
 
 function Reviews (){
+    const [ name, setName] = useState("")
+    const [ comment, setComment] = useState("")
+    const [ rating, setRating] = useState("")
+
+    const [ reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/reviews")
+        .then(r => r.json())
+        .then(data => setReviews(data));
+        
+    }, []);
+    
+    const reviewComponent = reviews.map (review => {
+        return <ReviewCard key={review.id} {...review} 
+        />
+    })
+
+
     return (
         <div>
             <h1>Reviews</h1>
-            
+            <form onSubmit={null} className="">
+        <h3>Tell Us What You Think!</h3>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Your Name..."
+          className="input-text"
+          
+        />
+        <br />
+        <input
+          type="text"
+          name="Comment"
+          placeholder="..."
+          className="input-text"
+      
+        />
+        <br />
+        <input
+          type="text"
+          name="Rating"
+          placeholder="..."
+          className="input-text"
+      
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="Submit Review"
+          className="submit"
+        />
+      </form>
+      {reviewComponent}
         </div>
     )
 }
