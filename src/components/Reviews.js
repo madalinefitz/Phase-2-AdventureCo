2a0492d3c5aee8d34fb22b24daeab21becbb74a8
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-07 11:38:38 -0500

    small edits

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -11,96 +11,85 @@ function Reviews (){
     useEffect(() => {
         fetch("http://localhost:3001/reviews")
         .then(r => r.json())
-        .then(data => setReviews(data));
-        
-    }, []);
+        .then(data => setReviews(data))   
+    }, [])
     
     const reviewComponent = reviews.map (review => {
-        return <ReviewCard key={review.id} {...review} 
-        />
-    })
+        return <ReviewCard key={review.id} {...review} />
+        })
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
+    const handleNameChange = (e) => setName(e.target.value)
+    const handleCommentChange = (e) => setComment(e.target.value)
+    const handleRatingChange = (e) => setRating(e.target.value)
 
-    const handleCommentChange = (e) => {
-        setComment(e.target.value);
-    };
-
-    const handleRatingChange = (e) => {
-        setRating(e.target.value);
-    };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
+
         const newReview = {
             name: name,
             comment: comment,
             rating: rating,
-        };
-    fetch("http://localhost:3001/reviews", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(newReview),
-    })
-       .then((r) => r.json())
-       .then((data) => {
-        setReviews([...reviews, data]);
-        setName("");
-        setComment("");
-        setRating("");
-       });
-    };
+        }
+    
+        fetch("http://localhost:3001/reviews", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(newReview),
+        })
+            .then((r) => r.json())
+            .then((data) => {
+                setReviews([...reviews, data])
+                setName("")
+                setComment("")
+                setRating("")
+        })
+    }
 
 
     return (
         <div>
             <h1>Reviews</h1>
             <form onSubmit={handleSubmit} className="">
-            <h3>Tell Us What You Think!</h3>
-            <input
-            type="text"
-            name="name"
-            placeholder="Enter Your Name..."
-            className="input-text"
-            value={name}
-            onChange={handleNameChange}
+                <h3>Tell Us What You Think!</h3>
+                <input
+                    type="text"
+                    label="Name"
+                    placeholder="Enter Your Name..."
+                    value={name}
+                    onChange={handleNameChange}
+                
+                />
+                <br />
+                <input
+                    type="text"
+                    placeholder="Enter review..."
+                    value={comment}
+                    onChange={handleCommentChange}
             
-            />
-            <br />
-            <input
-            type="text"
-            name="Comment"
-            placeholder="Enter review..."
-            className="input-text"
-            value={comment}
-            onChange={handleCommentChange}
-        
-            />
-            <br />
-            <select
-            name="rating"
-            className="input-text"
-            value={rating}
-            onChange={handleRatingChange}
-            >
-                <option value="">Select a rating</option>   
-                <option value="⭐"> ⭐</option>
-                <option value="⭐⭐"> ⭐⭐</option>
-                <option value="⭐⭐⭐"> ⭐⭐⭐</option>
-                <option value="⭐⭐⭐⭐"> ⭐⭐⭐⭐</option>
-                <option value="⭐⭐⭐⭐⭐"> ⭐⭐⭐⭐⭐</option>
-            </select>
-            <br />
-            <input
-            type="submit"
-            name="submit"
-            value="Submit Review"
-            className="submit"
-            />
-      </form>
-      {reviewComponent}
+                />
+                <br />
+                <select
+                    value={rating}
+                    onChange={handleRatingChange}
+                >
+                        <option value="">Select a rating</option>   
+                        <option value="⭐"> ⭐</option>
+                        <option value="⭐⭐"> ⭐⭐</option>
+                        <option value="⭐⭐⭐"> ⭐⭐⭐</option>
+                        <option value="⭐⭐⭐⭐"> ⭐⭐⭐⭐</option>
+                        <option value="⭐⭐⭐⭐⭐"> ⭐⭐⭐⭐⭐</option>
+                        
+                </select>
+                <br />
+                <input
+                    type="submit"
+                    value="Submit Review"
+                />
+            </form>
+      
+            {reviewComponent}
+
         </div>
     );
 }
