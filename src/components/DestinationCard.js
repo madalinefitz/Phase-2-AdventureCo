3ae99ca129b269f18362797d0fde15d155227112
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-07 12:46:20 -0500

    toggle on photos

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,14 +1,37 @@
+import { Image } from 'semantic-ui-react';
+import { useState } from 'react'
 
+const Front = ({country, image}) => {
+    return (
+        <div>
+            <Image size='large' circular src={image} alt={country}/>
+        </div>
+    )
+}
 
-function DestinationCard({country, capital, food, image}){
-
- 
-    return(
+const Back=({country, capital, food})=>{
+    return (
         <div>
             <h1>{country}</h1>
             <h2>{capital}</h2>
             <h2>{food}</h2>
-            <img src={image} alt={country}/>
+        </div>
+    )
+}
+
+
+function DestinationCard({country, capital, food, image}){
+    const [showPhoto, setShowPhoto] = useState(true)
+    const togglePhoto = () => {
+        setShowPhoto(showPhoto => !showPhoto)
+    }
+    
+ 
+    return(
+        <div onClick={togglePhoto}>
+            {showPhoto ? 
+            <Front country={country} image={image}/> : 
+            <Back country={country} food={food} capital={capital}/>}
         </div>
     )
 }
